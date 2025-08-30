d1530e5e4db4c1e7056a4f401e1c6cfc0ebbed12
gargatulya <91016711+gargatulya@users.noreply.github.com>
2025-08-30 20:18:19 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header">
             <div className="headerLeft">
                 <Link to="/"><img className="header__icon" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/black-and-white-movie-logo-design-template-040178fdca7dbedb4527b6eb4284a15d_screen.jpg?ts=1585497731" /></Link>
-               <Link to="https://gargatulya.github.io/Movie-advisor/" style={{textDecoration: "none"}}><span>Home</span></Link>
+               <Link to="/Movie-advisor/" style={{textDecoration: "none"}}><span>Home</span></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
